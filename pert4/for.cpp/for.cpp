544bb109fafd7885c7a16a8da3465de06d6a1a64
vaniiiiar <vaniarahmawati252@student.esaunggul.ac.id>
2024-04-25 16:19:24 +0700

    pert4 array

---
+++ pert4/for.cpp/for.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << " Iteration Statment " << endl;
+    cout << " Penggunaan For " << endl;
+    int a = 1
+    //apa bedannya int diluar for dengan int didalam for ?
+
+    for (int a = 1; a<=5; a++){
+        cout << a << endl
+    }
+
+    return 0;
+}
