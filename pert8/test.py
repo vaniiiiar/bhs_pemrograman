e575fc0153fb2ac1e48467f65071f0f0a19af2c5
vaniiiiar <vaniarahmawati252@student.esaunggul.ac.id>
2024-05-15 15:27:22 +0700

    fresh commit

---
+++ pert8/test.py
@@ -0,0 +1,12 @@
+from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
+import numpy as np
+fig = px.line(
+    x = np.array([0,6]),
+    y = np.array([0,250]),
+    title ="sample", height=325
+)
+app = Dash()
+app.layout = html.Div([
+    dcc.Graph(figure=fig)
+app.run_server(debug=True, use_reloader=False)
